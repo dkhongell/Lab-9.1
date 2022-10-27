ca33b70005b39e72904eaa0c0a2a02ef9b738fbe
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-10-27 16:26:06 -0400

    Add files via upload

---
+++ L.91.py
@@ -0,0 +1,8 @@
+#Brenna and Kalena
+def breakify(lines):
+    return"<br>".join(codes)
+
+lines=["Haiku frogs in snow",
+        "A limerick came from Nantucket",
+        "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+        
